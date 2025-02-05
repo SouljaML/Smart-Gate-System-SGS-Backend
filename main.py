e3e3cf74f845e01694c99d987476549fe480769f
ketumile <ketumilelesoli@gmail.com>
2025-02-05 12:01:06 +0200

    fixed errors in main

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from app.db.database import init_db
 from app.routes.gate_routes import router as gate_router
 from app.routes.user_routes import router as user_router
-from app.routes.external_data import secure_external_data
 
 
 app = FastAPI(title="Smart Gate System API", version="1.0.0")
@@ -11,7 +10,6 @@ app = FastAPI(title="Smart Gate System API", version="1.0.0")
 # Include routes
 app.include_router(gate_router)
 app.include_router(user_router, prefix="/users", tags=["Users"])
-app.include_router(secure_external_data.router)
 
 
 @app.get("/")
