0baaef53d105a9af2d1c4e7e3f5e1d78b1c5019a
ketumile <ketumilelesoli@gmail.com>
2025-04-01 11:00:46 +0200

    Set correct environmental variable

--- app/Security/security.py
+++ app/Security/security.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
-print(os.getenv(API_KEY))
+key = os.getenv("API_KEY")
+print(key)
 
 api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=False)
 
@@ -18,9 +18,9 @@ def verify_api_key(api_key: str = Security(api_key_header)):
     :return:
     """
     print(f"Received API Key: {api_key}")
-    print(f"Expected API Key: {API_KEY}")
+    print(f"Expected API Key: {key}")
 
-    if api_key != API_KEY:
+    if api_key != key:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid API Key"
